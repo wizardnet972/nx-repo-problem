0967cbcba98fd730a24da01442cfaa69d1518d99
Shlomi Levi <wizardnet972@gmail.com>
2024-09-24 11:57:56 +0300

    Initial commit

---
+++ apps/nx-repo-problem-ui/jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  displayName: 'nx-repo-problem-ui',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../coverage/apps/nx-repo-problem-ui',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
